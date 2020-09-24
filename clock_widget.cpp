7e632549b2f85aa3f39377147ed66dbfbbaf7bd3
krage56 <ivan.proskuryakov.2001@yandex.ru>
2020-09-24 10:29:53 +0300

    First working ver

--- clock_widget.cpp
+++ clock_widget.cpp
@@ -2,7 +2,8 @@
 
 #include <QPainter>
 #include <QTime>
-#include <iostream>
+#include <QtMath>
+
 
 Clock::Clock(QWidget* parent)
 	: QWidget(parent)
@@ -11,17 +12,17 @@ Clock::Clock(QWidget* parent)
 	, _seconds(0)
 {
 	setMinimumSize(100,100);
-	startTimer(1000);
+    //startTimer(1000);
 }
 
-void Clock::timerEvent(QTimerEvent*)
-{
-	QTime t  = QTime::currentTime();
-	_hours   = t.hour();
-	_minutes = t.minute();
-	_seconds = t.second();
-	update();
-}
+//void Clock::timerEvent(QTimerEvent*)
+//{
+//	QTime t  = QTime::currentTime();
+//	_hours   = t.hour();
+//	_minutes = t.minute();
+//	_seconds = t.second();
+//	update();
+//}
 
 
 void drawArrow(QPainter& p)
@@ -35,14 +36,76 @@ void Clock::paintEvent(QPaintEvent*)
 {
 	QPainter p(this);
 	QPoint center(width()/2, height()/2);
-	p.translate(center);
 
-	// seconds
-	p.save();
-	p.rotate(_seconds * 6);
-	p.scale(10,10);
-	p.setPen(Qt::blue);
+	p.translate(center);
+    p.save();
+    qreal angleStep = 30;
+    qreal correction_x = 0, correction_y = 0;
+    for (int i = 0; i < 12; ++i){
+        p.save();
+        p.rotate(i * angleStep);
+        p.restore();
+        if(i >= 4 && i < 6){
+            correction_x += 5;
+            correction_y += 5;
+        }
+//        if(i >= 6 && i < 8){
+
+//        }
+        if(i == 6){
+            correction_x -= 5;
+            correction_y -= 5;
+        }
+        if(i == 8){
+            correction_y -= 10;
+            correction_x += 10;
+        }
+        if (i == 11){
+            correction_x = 0;
+        }
+        double xpos=(110+correction_x)*qCos((i * angleStep)/360.*2.*M_PI);
+        double ypos=(110+correction_y)*qSin((i * angleStep)/360.*2.*M_PI);
+        p.drawText(xpos, ypos, QString::number((i+2)%12+1));
+
+    }
+
+    p.restore();
+    p.setPen(Qt::black);
+    p.drawEllipse(-100,-100,200,200);
+
+    // seconds
+    p.save();
+    p.rotate((_seconds - 30) * 6);
+    p.scale(10,10);
+    QPen arrow(Qt::blue);
+    arrow.setWidth(0);
+    p.setPen(arrow);
 	drawArrow(p);
-	p.restore();
+    p.restore();
+
+    //minutes
+    p.save();
+    p.rotate(_minutes * 6);
+    p.scale(8,8);
+    p.setPen(arrow);
+    drawArrow(p);
+    p.restore();
+    for (int j = 0; j < 60; ++j) {
+        if ((j % 5) != 0)
+            p.drawLine(92, 0, 96, 0);
+        p.rotate(6.0);
+    }
+
+    //hours
+    p.save();
+    p.rotate(_hours * 6);
+    p.scale(6,6);
+    p.setPen(arrow);
+    drawArrow(p);
+    p.restore();
+    for (int j = 0; j < 12; ++j) {
+        p.drawLine(88, 0, 96, 0);
+        p.rotate(30.0);
+    }
 }
 
